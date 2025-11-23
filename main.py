ebc8164f24bffe6209af3044ec918370e2efce10
Habiba Eid <159670923+HabibaEid2@users.noreply.github.com>
2025-11-24 00:11:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+import os 
+import cv2 
+from matplotlib import pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.models import Model
+from keras.layers import Layer , Conv2D , Dense , MaxPooling2D , Input, Flatten
